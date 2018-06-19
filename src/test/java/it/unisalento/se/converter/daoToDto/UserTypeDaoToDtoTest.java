76a5464ca760a1a2a191b43665d9a34c6e726b9f
Leonardo <leonardo.capoccia@hotmail.it>
2018-06-19 13:56:29 +0200

    Add user test for dao and dto converter

---
+++ src/test/java/it/unisalento/se/converter/daoToDto/UserTypeDaoToDtoTest.java
@@ -0,0 +1,40 @@
+package it.unisalento.se.converter.daoToDto;
+
+import it.unisalento.se.dao.UserType;
+import it.unisalento.se.dto.UserTypeModel;
+import it.unisalento.se.exceptions.UserTypeNotSupported;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTypeDaoToDtoTest {
+
+    @Test
+    public void convertWithValidType() throws UserTypeNotSupported {
+        UserType profType = new UserType();
+        profType.setName("PROFESSOR");
+
+        UserType studentType = new UserType();
+        studentType.setName("STUDENT");
+
+        UserType secretariatType = new UserType();
+        secretariatType.setName("SECRETARIAT");
+
+        UserTypeModel profTypeModel = UserTypeDaoToDto.convert(profType);
+        UserTypeModel studentTypeModel = UserTypeDaoToDto.convert(studentType);
+        UserTypeModel secretariatTypeModel = UserTypeDaoToDto.convert(secretariatType);
+
+        assertEquals(UserTypeModel.PROFESSOR, profTypeModel);
+        assertEquals(UserTypeModel.STUDENT, studentTypeModel);
+        assertEquals(UserTypeModel.SECRETARIAT, secretariatTypeModel);
+    }
+
+    @Test(expected = UserTypeNotSupported.class)
+    public void convertWithInvalidType() throws UserTypeNotSupported {
+        UserType type = new UserType();
+        type.setName("Invalid");
+
+        UserTypeModel m = UserTypeDaoToDto.convert(type);
+    }
+}
