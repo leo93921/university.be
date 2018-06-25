b4d4011aee73befa84c46806805178d97293d73f
Leonardo <leonardo.capoccia@hotmail.it>
2018-06-25 13:41:16 +0200

    Constants are now final

--- src/main/java/it/unisalento/se/common/Constants.java
+++ src/main/java/it/unisalento/se/common/Constants.java
@@ -2,8 +2,8 @@ package it.unisalento.se.common;
 
 public class Constants {
 
-    public static String STUDENT = "STUDENT";
-    public static String PROFESSOR = "PROFESSOR";
-    public static String SECRETARIAT = "SECRETARIAT";
+    public static final String STUDENT = "STUDENT";
+    public static final String PROFESSOR = "PROFESSOR";
+    public static final String SECRETARIAT = "SECRETARIAT";
 
 }
