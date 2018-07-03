6f4cdf7ace991a20f644761bd71661fe904ff3f8
Leonardo <leonardo.capoccia@hotmail.it>
2018-07-03 13:45:18 +0200

    REST for filtering the lessons

--- src/main/java/it/unisalento/se/api/rest/LessonRestController.java
+++ src/main/java/it/unisalento/se/api/rest/LessonRestController.java
@@ -4,10 +4,13 @@ package it.unisalento.se.api.rest;
 import it.unisalento.se.exceptions.LessonNotFoundException;
 import it.unisalento.se.exceptions.UserTypeNotSupported;
 import it.unisalento.se.iservices.ILessonService;
+import it.unisalento.se.models.LessonFilterModel;
 import it.unisalento.se.models.LessonModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/lesson")
@@ -26,6 +29,11 @@ public class LessonRestController {
     @PostMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public LessonModel saveLesson(@RequestBody LessonModel model) throws UserTypeNotSupported {
         return lessonService.saveLesson(model);
+    }
+
+    @PostMapping(value = "/filter", produces = MediaType.APPLICATION_JSON_UTF8_VALUE, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<LessonModel> getLessonByDate(@RequestBody LessonFilterModel filter) throws UserTypeNotSupported {
+        return lessonService.filterByTimeAndSubject(filter);
     }
 
 
--- src/main/java/it/unisalento/se/iservices/ILessonService.java
+++ src/main/java/it/unisalento/se/iservices/ILessonService.java
@@ -2,11 +2,16 @@ package it.unisalento.se.iservices;
 
 import it.unisalento.se.exceptions.LessonNotFoundException;
 import it.unisalento.se.exceptions.UserTypeNotSupported;
+import it.unisalento.se.models.LessonFilterModel;
 import it.unisalento.se.models.LessonModel;
+
+import java.util.List;
 
 public interface ILessonService {
 
     LessonModel getLessonByID(Integer ID) throws UserTypeNotSupported, LessonNotFoundException;
 
     LessonModel saveLesson(LessonModel model) throws UserTypeNotSupported;
+
+    List<LessonModel> filterByTimeAndSubject(LessonFilterModel filter) throws UserTypeNotSupported;
 }
