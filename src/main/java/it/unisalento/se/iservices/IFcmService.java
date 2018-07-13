914ea1e0c7f004474e7c8a3ececac1bb3282c212
Leonardo <leonardo.capoccia@hotmail.it>
2018-07-13 13:34:02 +0200

    Created service to send notifications to users subscribed to a topic

---
+++ src/main/java/it/unisalento/se/iservices/IFcmService.java
@@ -0,0 +1,10 @@
+package it.unisalento.se.iservices;
+
+import com.google.firebase.messaging.FirebaseMessagingException;
+
+import java.io.IOException;
+
+public interface IFcmService {
+
+    void sendMessageToTopic(String title, String body, String topicName) throws IOException, FirebaseMessagingException;
+}
